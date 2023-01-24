e8f85ea24bfd180461f178649a54dbeedf38dcdf
Safal Thapaliya <safalthapaliya@gmail.com>
2023-01-24 11:09:50 +0545

    yolov8 configs for brightfield sample

---
+++ configs/yolov8/model/brightfield_sample.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+
+def train_yolov8():
+    model = YOLO("yolov8s.pt")
+
+    model.train(
+        epochs=50,
+        batch=16,
+        data="configs/yolov8/data/brightfield_sample.yaml",
+        device=1,
+        name="yolov8_brightfield_sample_fold_1",
+        project="/mnt/Enterprise/safal/AI_assisted_microscopy_system/outputs/brightfield_sample/yolov8/fold_1/mmdetection_cysts",
+        seed=42,
+        lr0=0.01,
+        lrf=0.01,
+    )
+
+
+if __name__ == "__main__":
+    train_yolov8()
