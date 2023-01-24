b94f1041c4b828756787c78c15ee3eb09b841d3b
Safal Thapaliya <safalthapaliya@gmail.com>
2023-01-24 11:13:15 +0545

    adjusted lr for smartphone sample yolov8

--- configs/yolov8/model/smartphone_sample.py
+++ configs/yolov8/model/smartphone_sample.py
@@ -5,13 +5,15 @@ def train_yolov8():
     model = YOLO("yolov8s.pt")
 
     model.train(
-        epochs=24,
-        batch=8,
+        epochs=50,
+        batch=16,
         data="configs/yolov8/data/smartphone_sample.yaml",
         device=1,
-        name="yolov8_smartphone_sample",
+        name="yolov8_smartphone_sample_fold_1",
         project="/mnt/Enterprise/safal/AI_assisted_microscopy_system/outputs/smartphone_sample/yolov8/fold_1/mmdetection_cysts",
         seed=42,
+        lr0=0.001,
+        lrf=0.001,
     )
 
 
